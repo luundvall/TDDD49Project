8a0c5d56500ee6b0bc7b35d961b9cc7da7744692
Kalle <karlu403@student.liu.se>
2017-11-10 16:56:45 +0100

    added files

---
+++ UltimateTicTacToe/UltimateTicTacToe/BLL/Button.cs
@@ -0,0 +1,40 @@
+﻿namespace UltimateTicTacToe.BLL
+{
+    public class Button : InterfaceButton
+    {
+        private string isTaken;
+        private int id;
+        private int boardId;
+
+        public Button(int id, int boardId)
+        {
+            this.Id = id;
+            this.isTaken = "";
+            this.boardId = boardId;
+            
+        }
+
+        public int Id { get => id; set => id = value; }
+
+        public Button getButton()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int getBoardId()
+        {
+            return this.boardId;
+        }
+
+        public void SetIsTaken(string isTaken)
+        {
+            this.isTaken = isTaken;
+        }
+
+        public string GetIsTaken()
+        {
+            return this.isTaken;
+        }
+
+    }
+}
---
+++ UltimateTicTacToe/UltimateTicTacToe/BLL/SubBoard.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace UltimateTicTacToe.BLL
+{
+    class SubBoard : InterfaceBoard <SubBoard>
+    {
+        InterfaceBoard <UltimateBoard> ultimateBoard;
+
+        private int id;
+        private Button[,] buttonBoard = new Button[3, 3];
+        private bool activeBoard;
+
+        public SubBoard(int id)
+        {
+            this.activeBoard = false;
+            int buttonId = 0;
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    buttonId++;
+                    buttonBoard[row, column] = new Button(buttonId, id);
+                }
+            }
+            this.id = id;
+        }
+
+        public int Id { get => id; set => id = value; }
+
+        public bool getActiveBoard()
+        {
+            return this.activeBoard;
+        }
+        public void setActiveBoard(bool active)
+        {
+            this.activeBoard = active;
+        }
+
+        public SubBoard getBoardSelf()
+        {
+            return this;
+        }
+
+        public SubBoard getBoardById(int BoardId)
+        {
+
+            if (this.id == BoardId)
+            {
+                return this;
+
+            }
+            return null;
+               
+        }
+
+
+        public Button GetButton(int move)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    Button button = buttonBoard[row, column];
+                    if (button.Id.Equals(move))
+                    {
+                        return button;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public string getTypeOfBoard()
+        {
+            return "SubBoard";
+        }
+
+        public Button[,] getButtonList()
+        {
+            return this.buttonBoard;
+        }
+    }
+}
